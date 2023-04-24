60ea5caaad2a8157b03c2dfc23f1c31391f4f2d0
Lipton Barua <liptonbarua274@gmail.com>
2023-04-24 11:13:24 +0600

    project

--- Firebase/firebase.config.js
+++ Firebase/firebase.config.js
@@ -1,15 +1,19 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQuX0dequ_qlvWazqgDS0kWSXFTjSRQd0",
-  authDomain: "online-food-c7e55.firebaseapp.com",
-  projectId: "online-food-c7e55",
-  storageBucket: "online-food-c7e55.appspot.com",
-  messagingSenderId: "358241890908",
-  appId: "1:358241890908:web:16fe25a300a549f9946c99"
+  apiKey: "AIzaSyC_vtzrMH94NMOVVfpWpJ6xRLdCOwoRb2U",
+  authDomain: "online-food-b006d.firebaseapp.com",
+  projectId: "online-food-b006d",
+  storageBucket: "online-food-b006d.appspot.com",
+  messagingSenderId: "646018447787",
+  appId: "1:646018447787:web:6faa4c2aa10507ad1cfaee"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
